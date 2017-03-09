1075640717889274e688845c51d7d1546a9b4943
Viacheslav Tiahotenkov <vtiahotenkov@cogniance.com>
2017-03-09 14:37:35 +0200

    Setup Rx and Retrolambda dependencies

--- app/src/main/java/name/syndarin/githubbrowser/MainActivity.java
+++ app/src/main/java/name/syndarin/githubbrowser/MainActivity.java
@@ -1,13 +1,32 @@
 package name.syndarin.githubbrowser;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.EditText;
+
+import com.jakewharton.rxbinding2.widget.RxTextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String tag = MainActivity.class.getSimpleName();
+
+    @BindView(R.id.edit_username_input)
+    EditText editSearchInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        RxTextView.textChanges(editSearchInput)
+            .filter(input -> input.length() >= 3)
+            .map(input -> input.toString().toUpperCase())
+            .doOnNext(input -> Log.i(tag, "Input value " + input))
+            .subscribe();
     }
 }

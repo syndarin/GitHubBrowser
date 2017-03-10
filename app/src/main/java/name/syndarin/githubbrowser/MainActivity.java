d720a5cdabb8495d6b5a186c0f86ece58f8a6aa2
Viacheslav Tiahotenkov <vtiahotenkov@cogniance.com>
2017-03-10 14:09:07 +0200

    Request GitHub API search for users who match input given

--- app/src/main/java/name/syndarin/githubbrowser/MainActivity.java
+++ app/src/main/java/name/syndarin/githubbrowser/MainActivity.java
@@ -7,8 +7,18 @@ import android.widget.EditText;
 
 import com.jakewharton.rxbinding2.widget.RxTextView;
 
+import java.io.IOException;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import io.reactivex.Observable;
+import io.reactivex.ObservableSource;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.functions.Function;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,16 +27,48 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.edit_username_input)
     EditText editSearchInput;
 
+    OkHttpClient okHttpClient;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
+        okHttpClient = new OkHttpClient();
+
         RxTextView.textChanges(editSearchInput)
-            .filter(input -> input.length() >= 3)
-            .map(input -> input.toString().toUpperCase())
-            .doOnNext(input -> Log.i(tag, "Input value " + input))
+            .subscribeOn(Schedulers.io())
+
+            .filter(input -> {
+                Log.i(tag, "filter " + Thread.currentThread().getName());
+                return input.length() >= 3;
+            })
+
+            .observeOn(Schedulers.io())
+
+            .map(input -> {
+                Log.i(tag, "map " + Thread.currentThread().getName());
+                return input.toString().toLowerCase();
+            })
+
+            .flatMap((Function<String, ObservableSource<?>>) s -> {
+                Log.i(tag, "flatMap " + Thread.currentThread().getName());
+                String url = "https://api.github.com/search/users?q=" + s;
+                Request request = new Request.Builder().url(url).build();
+                return Observable.just(okHttpClient.newCall(request).execute());
+            })
+
+            .cast(Response.class)
+
+            .observeOn(AndroidSchedulers.mainThread())
+
+            .doOnNext(result -> {
+                Log.i(tag, "doOnNext " + Thread.currentThread().getName());
+                Log.i(tag, String.valueOf(result.body().toString()));
+            })
+
+            .doOnError(Throwable::printStackTrace)
             .subscribe();
     }
 }

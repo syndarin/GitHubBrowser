cad75021a534bdfa267bd6acb20441cd459265ed
Viacheslav Tiahotenkov <vtiahotenkov@cogniance.com>
2017-03-17 17:17:04 +0200

    Display some stats on UserProfileActivity

--- app/src/main/java/name/syndarin/githubbrowser/viewmodels/UserProfileActivityViewModel.java
+++ app/src/main/java/name/syndarin/githubbrowser/viewmodels/UserProfileActivityViewModel.java
@@ -1,9 +1,8 @@
 package name.syndarin.githubbrowser.viewmodels;
 
 import android.content.Context;
+import android.databinding.BaseObservable;
 import android.databinding.ObservableField;
-
-import com.bumptech.glide.Glide;
 
 import javax.inject.Inject;
 
@@ -11,6 +10,7 @@ import io.reactivex.Observable;
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
 import io.reactivex.schedulers.Schedulers;
+import name.syndarin.githubbrowser.R;
 import name.syndarin.githubbrowser.databinding.ActivityUserProfileBinding;
 import name.syndarin.githubbrowser.models.SearchModel;
 
@@ -18,7 +18,7 @@ import name.syndarin.githubbrowser.models.SearchModel;
  * Created by vtiahotenkov on 17.03.17.
  */
 
-public class UserProfileActivityViewModel {
+public class UserProfileActivityViewModel extends BaseObservable {
 
     @Inject
     SearchModel searchModel;
@@ -32,9 +32,18 @@ public class UserProfileActivityViewModel {
     public ObservableField<String> username;
     public ObservableField<String> avatarUrl;
 
+    public ObservableField<String> followersCount;
+    public ObservableField<String> followingCount;
+    public ObservableField<String> reposCount;
+    public ObservableField<String> gistsCount;
+
     public UserProfileActivityViewModel(ActivityUserProfileBinding binding, String url) {
         username = new ObservableField<>();
         avatarUrl = new ObservableField<>();
+        followersCount = new ObservableField<>();
+        followingCount = new ObservableField<>();
+        reposCount = new ObservableField<>();
+        gistsCount = new ObservableField<>();
 
         observableContentLoader = Observable.defer(() -> searchModel.getUserProfile(url))
                 .subscribeOn(Schedulers.io())
@@ -42,6 +51,10 @@ public class UserProfileActivityViewModel {
                 .doOnNext(user -> {
                     username.set(user.getName());
                     avatarUrl.set(user.getAvatarUrl());
+                    followersCount.set(context.getString(R.string.user_profile_followers_template, user.getFollowers()));
+                    followingCount.set(context.getString(R.string.user_profile_following_template, user.getFollowing()));
+                    reposCount.set(context.getString(R.string.user_profile_repos_template, user.getPublicRepos()));
+                    gistsCount.set(context.getString(R.string.user_profile_gists_template, user.getPublicGists()));
                 });
     }
 
